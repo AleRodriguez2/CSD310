d5471e30f07856204b24ff8c6ab4b83b30020439
alerodriguez2 <alerodriguez2@my365.bellevue.edu>
2023-06-16 16:09:31 -0400

    initial commit

---
+++ Module5/pytech_queries.py
@@ -0,0 +1,43 @@
+""" 
+    Title: pytech_queries.py
+    Author: Professor Krasso
+    Modified By: Alejandro Rodriguez    
+    Date: 10 July 2020
+    Modified on: 16 June 2023
+    Description: Test program for querying the students collection.
+"""
+
+""" import statements """
+from pymongo import MongoClient
+
+# MongoDB connection string 
+url = "mongodb+srv://admin:admin@cluster1.r5vioe7.mongodb.net/pytech?retryWrites=true&w=majority"
+
+# connect to the MongoDB cluster 
+client = MongoClient(url)
+
+# connect pytech database
+db = client.pytech
+
+# get the students collection 
+students = db.students
+
+# find all students in the collection 
+student_list = students.find({})
+
+# display message 
+print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+# loop over the collection and output the results 
+for doc in student_list:
+    print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+
+# find document by student_id
+bilbo = students.find_one({"student_id": "1008"})
+
+# output the results 
+print("\n  -- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
+print("  Student ID: " + bilbo["student_id"] + "\n  First Name: " + bilbo["first_name"] + "\n  Last Name: " + bilbo["last_name"] + "\n")
+
+# exit message 
+input("\n\n  End of program, press any key to continue...")
